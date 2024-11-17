0d6b3b49cf04b18920a5e1d3ea9ff94612b1254b
Mukand Kirshana <129943687+mukand-mapara@users.noreply.github.com>
2024-11-17 14:49:55 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <p className="text-decoration-none text-dark fs-5">Mukand Kirshna</p>
+              <a className="text-decoration-none text-dark fs-5">Mukand Kirshna</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/mukand-mapara" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
